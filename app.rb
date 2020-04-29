4a8e1771d2afd62f9a0b0ae893f217c691762d4e
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-04-29 06:44:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,21 +3,18 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
     configure do
-      enable :sessions
-      set :session_secret, "secret"
-    end
-    
-    get "/" do 
+        enable :sessions
+        set :session_secret, "clearly not a secret"
+      end
+
+      get '/' do 
         erb :index
-    end
-    
-    post '/checkout'
-      session[:item] = params[:item]
-      @session = @session
-      
-      erb :checkout
-    end 
+      end
 
+      post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
 
-
+        erb :checkout
+      end
 end
